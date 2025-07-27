f28a3bb818f44559a5434c1e8c9a7f256cde8e11
Nathaniel Bates <nathaniel.christopher.bates@gmail.com>
2025-07-27 02:07:52 +0100

    Initial

---
+++ src/comps/App.jsx
@@ -0,0 +1,12 @@
+
+
+const App = () => {
+    return (
+        <div>
+            <h1>Welcome to Our Wedding Photo Services</h1>
+            <p>Capture your special moments with us!</p>
+        </div>
+    );
+}
+
+export default App;
